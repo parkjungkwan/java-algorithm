c1912db4106ff8b59eabfdb5c2f3df1df1a07764
parkjungkwan <pakjkwan@gmail.com>
2022-10-11 17:36:12 +0900

    first commit

---
+++ 수학/비만도측정.java
@@ -0,0 +1,56 @@
+package 수학;
+/**
+키와 몸무게를 입력받아서 비만도를 측정하는 프로그램을 완성하시오.
+BMI 지수를 구하는 공식은 다음과 같다.
+BMI지수 = 몸무게(70kg) / (키(1.7m) * 키(1.7m))
+
+BMI 지수에 따른 결과는 다음과 같다.
+고도 비만 : 35 이상
+중(重)도 비만 (2단계 비만) : 30 - 34.9
+경도 비만 (1단계 비만) : 25 - 29.9
+과체중 : 23 - 24.9
+정상 : 18.5 - 22.9
+저체중 : 18.5 미만
+
+
+이름, 키, 몸무게를 입력받으면 다음과 같이 출력되도록 하시오.
+
+### 비만도 계산 ###
+***************************
+이름 키(cm) 몸무게(kg) 비만도
+***************************
+홍길동 170 79 정상
+***************************
+*/
+import java.util.*;
+class 비만도측정{
+    public static void main(String[] args) {
+        비만도측정 solution = new 비만도측정();
+        Scanner scanner = new  Scanner(System.in);
+        System.out.println("이름:");
+        String name = scanner.next();
+        System.out.println("키: ");
+        float cm = scanner.nextFloat(); 
+        System.out.println("몸무게: ");
+        float kg = scanner.nextFloat();
+        System.out.println(solution.solution(name, cm, kg));
+    }
+    String solution(String name,float cm,float kg){
+        String title = " ### 비만도 측정 ### ";
+        float m = cm / 100;
+        float bmi = kg / (m * m);
+        String biman = "";
+        if(bmi >= 35.0){biman = "고도 비만";}
+        else if(bmi >= 30.0){biman = "중(重)도 비만 (2단계 비만)";}
+        else if(bmi >= 25.0){biman = "경도 비만 (1단계 비만)";}
+        else if(bmi >= 23.0){biman = "과체중";}
+        else if(bmi >= 18.5){biman = "정상";}
+        else{biman = "저체중";}
+        return String.format(
+        "%s \n ***************************\n"
+        +"이름 키(cm) 몸무게(kg) 비만도\n"
+        +"***************************\n"
+        +"%s %.1f %.1f %s\n"
+        +"***************************\n", title, name, cm, kg, biman);
+    }
+}
---
+++ 수학/성적표.java
@@ -0,0 +1,57 @@
+package 수학;
+/**
+국어. 영어, 수학점수를 입력받아서 학점을 출력하는 프로그램을 완성하시오.
+각 과목 점수는 0 ~ 100 사이이다.
+평균에 따라 다음과 같이 학점이 결정된다
+90이상은 A학점
+80이상은 B학점
+70이상은 C학점
+60이상은 D학점
+50이상은 E학점
+그 이하는 F학점
+
+출력되는 결과는 다음과 같다.
+
+### 성적표 ###
+********************************
+이름 국어 영어 수학 총점 평균 학점
+*******************************
+홍길동 90 90 92 272 90.6 A
+********************************
+ */
+import java.util.*;
+class 성적표 {
+    public static void main(String[] args) {
+        성적표 solution = new 성적표();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("이름: ");
+        String name = scanner.next();
+        System.out.println("국어: ");
+        int ko = scanner.nextInt();
+        System.out.println("영어: ");
+        int en = scanner.nextInt();
+        System.out.println("수학: ");
+        int ma = scanner.nextInt();
+        System.out.println(solution.solution(name, ko, en, ma));
+        scanner.close();
+    }
+    String solution(String name,int ko,int en,int ma){
+        String title = "### 성적표 ###";
+        int tot = ko + en + ma;
+        float avg = tot / 3;
+        String grade = ""; 
+        if(avg >= 90){grade = "A";}
+        else if(avg >= 80){grade = "B";}
+        else if(avg >= 70){grade = "C";}
+        else if(avg >= 60){grade = "D";}
+        else if(avg >= 50){grade = "E";}
+        else{grade = "F";}
+        return String.format(
+        "%s \n ********************************\n"
+        +"이름 국어 영어 수학 총점 평균 학점\n"
+        +"*******************************\n"
+        +"%s %d %d %d %d %.1f %s학점\n"
+        +"********************************", title, name, ko, en, ma, tot, avg, grade);
+    }
+    
+}
